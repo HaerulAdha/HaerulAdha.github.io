70747a53b5bc9866f840f2883713cc11f8b78d23
HaerulAdha <haeruadaha@gmail.com>
2023-03-11 19:55:10 +0700

    membuat file portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+// Membuat tombol hamburger menu
+let hamburger = document.getElementsByClassName('hamburgerMenu');
+
+hamburger[0].addEventListener('click', function () {
+  //  eksekusi isi, belum di isi
+});
