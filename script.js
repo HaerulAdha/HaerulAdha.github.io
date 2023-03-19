747f98e6749ae7df769e2d7d20f972e664988ac4
HaerulAdha <haeruadaha@gmail.com>
2023-03-19 18:52:09 +0700

    menambah dan memodify file html dan css

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ let hamburger = document.getElementsByClassName('hamburgerMenu');
 
 hamburger[0].addEventListener('click', function () {
   //  eksekusi isi, belum di isi
+  let s = document.getElementsByTagName('ul');
+  s[0].classList.toggle('muncul');
 });
